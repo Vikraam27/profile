7f61537f5d342d02d6d87ff13e38953c0d3e4c78
Vikraam27 <ramvik36@gmail.com>
2022-06-03 13:33:00 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navigation = document.querySelector('.navigation');
+  menuToggle.addEventListener('click', () => {
+      menuToggle.classList.toggle('active');
+      navigation.classList.toggle('active');
+  });
+});
+
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+  });
+  
